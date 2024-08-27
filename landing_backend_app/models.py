ce5585428096ee9ce685fd2b4209d2513d075502
Yerassyliitu <34080@iitu.edu.kz>
2024-08-28 04:25:59 +0500

    fix: order null fields

--- landing_backend_app/models.py
+++ landing_backend_app/models.py
@@ -15,12 +15,12 @@ class MessengerTypes(models.Model):
 
 class Orders(models.Model):
     service_type = models.ForeignKey(ServiceTypes, on_delete=models.CASCADE, default=1)
-    name = models.CharField(max_length=255)
-    project_name = models.CharField(max_length=255)
-    messenger_type = models.ForeignKey(MessengerTypes, on_delete=models.CASCADE)
-    contact = models.CharField(max_length=255)
-    budget = models.IntegerField()
-    message = models.TextField()
+    name = models.CharField(max_length=255, null=False)
+    project_name = models.CharField(max_length=255, null=False)
+    messenger_type = models.ForeignKey(MessengerTypes, on_delete=models.CASCADE, default=1)
+    contact = models.CharField(max_length=255, null=False)
+    budget = models.IntegerField(null=False)
+    message = models.TextField(null=True)
 
     def __str__(self):
         return self.name + ' - ' + self.project_name
